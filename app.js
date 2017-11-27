13295250b134118470b36923f220c2e66ae5304d
sebashack <sebastian0092@gmail.com>
2017-11-26 21:26:52 -0500

    Removing unnecessary dependencies

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import express from 'express';
 import React from 'react';
-import homedir from 'homedir';
 import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 import { StaticRouter as Router } from 'react-router-dom';
@@ -12,10 +11,10 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', path.resolve(homedir(), 'Reason', 'reason_universal', 'views'));
+app.set('views', path.resolve(__dirname, 'views'));
 
 app
-  .use('/static', express.static(path.resolve(homedir(), 'Reason', 'reason_universal', 'public')))
+  .use('/static', express.static(path.resolve(__dirname, 'public')))
   .get('/', (req, res) => {
     res.render('index.pug');
   });
